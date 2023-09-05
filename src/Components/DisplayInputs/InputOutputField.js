7ee7c7e99fabcefe4c3364d5a2dd795c891dc5a4
Your Name <you@example.com>
2023-09-05 06:22:51 -0400

    adebugging

--- src/Components/DisplayInputs/InputOutputField.js
+++ src/Components/DisplayInputs/InputOutputField.js
@@ -1,6 +1,3 @@
-export const InputOutputField = ({ input, output }) => {
+export const InputOutputField = ({ input }) => {
   return <input id="display" value={input ? input : 0} />;
 };
-
-// InputOutputField.defaultProps = { result: 0 };
-// InputOutputField.defaultProps = { result: props.result };
